e4f274bd4981bdb089a7855b015f053f6900c82b
Nguyen Tan Cao Hao <haolq0711@gmail.com>
2025-07-18 20:36:11 +0700

    Add API get students information with mock data

---
+++ src/main/java/com/example/education_management_api/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.education_management_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @GetMapping("/all")
+    public String getAllStudents() {
+        return "List of all students";
+    }
+
+    @GetMapping("/student-count")
+    public String getStudentCount() {
+        return "Total number of students";
+    }
+
+    @GetMapping("/student-details")
+    public String getStudentDetails() {
+        return "Ho ten: Nguyen Tan Cao Hao - Dia chi: Da Nang";
+    }
+}
