30f88a85143c9a417f99daa547526921382a2c03
Hao Nguyen <haolq0711@gmail.com>
2026-03-11 20:54:24 +0700

    Update CORS

--- src/main/java/com/example/education_management_api/WebConfig.java
+++ src/main/java/com/example/education_management_api/WebConfig.java
@@ -9,7 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://education-management-website-gmo2.onrender.com"
+                )
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
---
+++ src/main/java/com/example/education_management_api/config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.example.education_management_api.config;
+
+public class WebConfig {
+}
